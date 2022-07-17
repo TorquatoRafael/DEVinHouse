5829ae63541894261bd795a4fe9e1cb307de00b4
Rafael <rafastorquato@hotmail.com>
2022-07-17 19:00:21 -0300

    exercicio6

--- modulo1/semana3/Ex 4 a 9/script.js
+++ modulo1/semana3/Ex 4 a 9/script.js
@@ -1,21 +1,59 @@
-var objeto1 = {
-    nome: 'John', sobrenome: 'Stuart', idade: '16', setor: 'camarote'
-}
-var objeto2 = {
-    nome: 'Julia', sobrenome: 'Perdona', idade: '20', setor: 'camarote'
-}
-var objeto3 = {
-    nome: 'Rafael', sobrenome: 'Elias', idade: '57', setor: 'pista'
-}
-var objeto4 = {
-    nome: 'João Eduardo', sobrenome: 'Robert', idade: '37', setor: 'pista'
-}
-var objeto5 = {
-    nome: 'Danielly', sobrenome: 'Luz', idade: '15', setor: 'arquibancada'
-}
-var objeto6 = {
-    nome: 'Edinara', sobrenome: 'Dias', idade: '67', setor: 'arquibancada'
-}
+var convidado1 = {
+  nome: "John",
+  sobrenome: "Stuart",
+  idade: 16,
+  setor: "camarote",
+};
+var convidado2 = {
+  nome: "Julia",
+  sobrenome: "Perdona",
+  idade: 20,
+  setor: "camarote",
+};
+var convidado3 = {
+  nome: "Rafael",
+  sobrenome: "Elias",
+  idade: 57,
+  setor: "pista",
+};
+var convidado4 = {
+  nome: "João Eduardo",
+  sobrenome: "Robert",
+  idade: 37,
+  setor: "pista",
+};
+var convidado5 = {
+  nome: "Danielly",
+  sobrenome: "Luz",
+  idade: 15,
+  setor: "arquibancada",
+};
+var convidado6 = {
+  nome: "Edinara",
+  sobrenome: "Dias",
+  idade: 67,
+  setor: "arquibancada",
+};
 
-var listaDeConvidados = [objeto1, objeto2, objeto3, objeto4, objeto5, objeto6]
-console.log(lista)
+var listaDeConvidados = [
+  convidado1,
+  convidado2,
+  convidado3,
+  convidado4,
+  convidado5,
+  convidado6,
+];
+
+function liberarBebidas(lista) {
+  var resultado = lista.map((convidado) => {
+    if (convidado.idade >= 18) {
+      convidado.openBar = "Sim";
+    }
+    convidado.openBar = "Não";
+
+    return convidado;
+  });
+  return resultado;
+}
+var convidadosBebidasLiberadas = liberarBebidas(listaDeConvidados);
+console.log(listaDeConvidados);
