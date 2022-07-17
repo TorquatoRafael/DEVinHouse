8e348ab3e269e22f7197a3b099837b5c6b322302
Rafael <rafastorquato@hotmail.com>
2022-07-17 14:29:02 -0300

    exercicio4

---
+++ modulo1/semana3/[M1S03] Ex 4/script.js
@@ -0,0 +1,19 @@
+var objeto1 = {
+    nome: 'John', sobrenome: 'Stuart', idade: '16', setor: 'camarote'
+}
+var objeto2 = {
+    nome: 'Julia', sobrenome: 'Perdona', idade: '20', setor: 'camarote'
+}
+var objeto3 = {
+    nome: 'Rafael', sobrenome: 'Elias', idade: '57', setor: 'pista'
+}
+var objeto4 = {
+    nome: 'João Eduardo', sobrenome: 'Robert', idade: '37', setor: 'pista'
+}
+var objeto5 = {
+    nome: 'Danielly', sobrenome: 'Luz', idade: '15', setor: 'arquibancada'
+}
+var objeto5 = {
+    nome: 'Edinara', sobrenome: 'Dias', idade: '67', setor: 'arquibancada'
+}
+
