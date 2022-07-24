3e67586429a9baefc1923d623e82eac24afc6fb7
Rafael <rafastorquato@hotmail.com>
2022-07-24 09:36:00 -0300

    estruturaPastas

--- modulo1/semana4/[M1S04] - Ex 1 ao 7/index.js
+++ modulo1/semana4/[M1S04] - Ex 1 ao 7/index.js
@@ -1,1 +1,22 @@
-var select = document.querySelect('#operacao')
+var btnConfirmar = document.query('button');
+var select = document.querySelector('#clientes');
+
+var contasClientes = [
+    {
+    id: 1,
+    nome: 'Cliente 01',
+    saldo: 500,
+    },
+    {
+    id: 2,
+    nome: 'Cliente 02',
+    saldo: 3000,
+    },
+    {
+    id: 3,
+    nome: 'Cliente 03',
+    saldo: 5000,
+    },
+    ];
+
+btnConfirmar.addEventListener('click', () )
